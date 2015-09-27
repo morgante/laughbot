c13984ae3c3ffbfeb01661d2fcb9b4e9ea376b1a
Morgante Pell <morgante.pell@morgante.net>
2015-09-27 12:40:54 +0400

    upgrade react router to 1.0.0rc1

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
 import 'babel-core/polyfill';
 import React from 'react';
-import BrowserHistory from 'react-router/lib/BrowserHistory';
 import { Provider } from 'react-redux';
-import { Router, Route } from 'react-router';
+import { Router, Route, IndexRoute } from 'react-router';
 import configureStore from './store/configureStore';
 import App from './components/App';
 import HomePage from './components/pages/HomePage';
 import CounterPage from './components/pages/CounterPage';
+import history from './router/history';
 
-const history = new BrowserHistory();
 const store = configureStore();
 
 React.render(
   <Provider store={store}>
     {() =>
       <Router history={history}>
-        <Route component={App}>
-	        <Route path="/" component={HomePage} />
-	        <Route path="/counter" component={CounterPage} />
-	        <Route path="/home" component={HomePage} />
+        <Route path="/" component={App}>
+          <IndexRoute component={HomePage} />
+	        <Route path="counter" component={CounterPage} />
+	        <Route path="home" component={HomePage} />
         </Route>
       </Router>
     }
